130f6e19a97c2d4b0380a4ce8fc285315dc9f086
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2024-04-27 15:06:16 +0530

    use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import { Greet } from "./components/Greet";
 
@@ -7,9 +8,16 @@ function App() {
     last: "Shree",
   };
 
+  const [color, setColor] = useState(false);
+  
+  const handleClick = () => {
+    setColor(true);
+  };
+  
   return (
-    <div className="App">
+    <div className="App" style={{ background: color ? "pink" : "yellow"}}>
       <Greet language="Typescript" value={20} period={false} name={nameData} />
+      <button onClick={handleClick}>COLOR</button>
     </div>
   );
 }
--- src/docs/notes.md
+++ src/docs/notes.md
@@ -1,5 +1,9 @@
 # Basic Props
 
-1. TYpe annotation
+1. Type annotation
 2. data types : string , number , boolean, object
 3. basic props 
+
+# State 
+
+1. basic use state 
